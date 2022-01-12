51997abb48d372b01141c88e2aaf9f98eec929e8
laizhi <laizhi727@126.com>
2022-01-12 20:48:05 +0800

    [feat] Export all from legato-dnd

--- index.ts
+++ index.ts
@@ -2,4 +2,6 @@ import DragContainer from './src/Container'
 import Draggable from './src/Draggable'
 import Handler from './src/Handler'
 export * from './src/Container'
+export * from 'legato-dnd'
+
 export { DragContainer, Draggable, Handler }
